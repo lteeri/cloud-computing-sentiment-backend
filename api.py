544ce5e054dd18172a9c4f2b6b4dba48d9e35c28
lteeri <leiniteeri@gmail.com>
2026-03-10 18:51:42 +0200

    got the model to work!

--- api.py
+++ api.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import pickle
 
 app = Flask(__name__)
+
+# bringing the sentiment analysis tool into the backend using pickle
+with open("sentiment_model_sklearn_1.6.1.pkl", "rb") as file:
+    sentiment_model = pickle.load(file)
+
+print(type(sentiment_model))
+
+print("should be positive: ", sentiment_model.predict(["Today was a great day."]))
+print("should be neutral: ", sentiment_model.predict(["I have strated building the backend."]))
+print("should be negative: ", sentiment_model.predict(["Some day my dog will die."]))
 
 @app.route("/")
 def hello_world():
